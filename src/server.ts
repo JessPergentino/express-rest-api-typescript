036f99b471664375136f1762f98202f5d2461e9f
Jessica Pergentino <jessicapergentino@hotmail.com>
2021-11-16 07:53:20 -0300

    WIP: Configurar e integrar o ESLint e o Prettier #1 - instalação e configuração do ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,15 @@
-import express from 'express'
+import express, { Router, Request, Response } from 'express'
 
-import { Router, Request, Response } from 'express';
-
-const app = express();
+const app = express()
 
 const route = Router()
 
 app.use(express.json())
 
-route.get('/', (req: Request, res: Response) => {
+route.get('/', (_req: Request, res: Response) => {
   res.json({ message: 'hello world with Typescript' })
 })
 
 app.use(route)
-
 
 app.listen(3000, () => 'server running on port 3000')
